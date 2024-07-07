3719a160b1e67e5d0054552baa5a258ba46dc42b
domenicolaporta00 <s1095492@studenti.univpm.it>
2024-07-07 19:47:56 +0200

    Modificati nome e logo.

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GreenMarket',
+      title: 'GreenMarket Flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
