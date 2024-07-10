eba0445e66e52ff877c60f977d8931cad3e48bbe
domenicolaporta00 <s1095492@studenti.univpm.it>
2024-07-10 21:44:28 +0200

    Implementato collegamento con db.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,14 @@ import 'splash_screen.dart';
 
 import 'package:provider/provider.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
